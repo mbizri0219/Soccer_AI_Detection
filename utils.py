1e88ec23df3a6ef4a7c2b12046434120dde10102
mbizri0219 <mhbizri@yahoo.com>
2025-12-03 18:31:20 -0700

    editing the utils.py file

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import logging
-
+import cv2
 #This file is for methods which help with calculating varying measurements and for setting different 
 # or designs based on class
 
@@ -20,36 +20,6 @@ def bbox_center_bottom(x1, y1, x2, y2):
     y_bottom = int(y2)
     return x_center, y_bottom
 
-#defining the color for different classes
-def class_color(class_id, color_dict=None):
-    if color_dict is None:
-        logging.error("color_dict is None, please define the color for each class")
-        raise ValueError("color_dict is None, please define the color for each class")
-    elif class_id not in color_dict:
-        logging.error(f"class_id {class_id} not found in color_dict, please define the color for this class")
-        raise ValueError(f"class_id {class_id} not found in color_dict, please define the color for this class")
-    else:
-        color = color_dict.get(class_id)
-        return color
-
-#Draw Arc Under Player
-def draw_player_arc(frame, center, radius, start_angle, end_angle, color, thickness=2, angle=0, x1, x2, y1, y2):
-    width = bbox_width(x1, x2)
-    center = bbox_center_bottom(x1, y1, x2, y2)
-    color = class_color(class_id)
-    
-           cv2.ellipse(
-            frame, 
-            center=(center), 
-            axes=int(width), int(.35*(width)), 
-            angle=0,
-            startAngle=-45, 
-            endAngle=250, 
-            color=color, 
-            thickness=2,
-            lineType=cv2.LINE_4)
-    
-    
     # Main processing function:
 
 # process_frame(frame, model, class_colors) - Runs inference and draws annotations
@@ -60,5 +30,44 @@ def draw_player_arc(frame, center, radius, start_angle, end_angle, color, thickn
 # Call process_frame() for each frame
 
 class VideoProcessor:
-    def __init__()
+    def __init__(self, model, colors_dict=None):
+        self.model = model
+        self.colors_dict = colors_dict
     
+    #defining the color for different classes
+    def class_color(self, class_id):
+    if self.colors_dict is None:
+        logging.error("color_dict is None, please define the color for each class")
+        raise ValueError("color_dict is None, please define the color for each class")
+    elif class_id not in color_dict:
+        logging.error(f"class_id {class_id} not found in color_dict, please define the color for this class")
+        raise ValueError(f"class_id {class_id} not found in color_dict, please define the color for this class")
+    else:
+        color = self.colors_dict.get(class_id)
+        return color
+    
+    #Draw Arc Under Player
+    def draw_player_arc(self, frame, class_id, radius, start_angle, end_angle, thickness=2, angle=0, x1, x2, y1, y2):
+    width = bbox_width(x1, x2)
+    center = bbox_center_bottom(x1, y1, x2, y2)
+    color = class_color(class_id, self.colors_dict)
+    
+    cv2.ellipse(
+    frame, 
+    center=(center), 
+    axes=int(width), int(.35*(width)), 
+    angle=0,
+    startAngle=-45, 
+    endAngle=250, 
+    color=color, 
+    thickness=2,
+    lineType=cv2.LINE_4)
+    
+    
+    def process_frame(self, frame):
+        results = self.model.predict(frame)
+        detections = results[0].boxes.xyxy
+        detections_class = results[0].boxes.cls
+        for detection, detection_class in zip(detections, detections_class):
+            x1, y1, x2, y2 = detection.cpu().numpy().astype(int)
+            color = class_color[int(detection_class)]
